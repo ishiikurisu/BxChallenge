2dc403e10b9ac25fed85bb8466ccd977014c3ae9
Cris Jr <crisjr@pm.me>
2021-04-26 11:48:23 -0300

    Sketch trade fairness verification

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -1,3 +1,2 @@
 class TradeController < ActionController::Base
-  
 end
--- test/controllers/trade_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TradeTest < ActiveSupport::TestCase
-  test "unit tests are running" do
-    assert true
-  end
-end
