fd502e71428683faa432cf6fb367b66a61d697bc
Cris Jr <crisjr@pm.me>
2021-04-26 16:07:38 -0300

    Connect ends

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,15 @@
+require "json"
+
 class MainController < ActionController::Base
   def index
   end
 
-  def fair inlet
+  def fair
+    lists = JSON.parse(params.as_json["parties"])
+    is_fair = PokemonInfo.is_trade_fair? lists[0], lists[1]
     data = {
-      :error => "not implemented",
+      :error => nil,
+      :result => is_fair,
     }
     render :json => data
   end
