2ac2ab6ca79ce4183d47f5e7052cc533f9335264
Cris Jr <crisjr@pm.me>
2021-04-27 11:22:45 -0300

    Update models and controllers to save and retrive trades

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,15 +2,21 @@ require "json"
 
 class MainController < ActionController::Base
   def index
+    @trades = Trade.order("created_at DESC").limit(5).map{ |x| x.to_view }
   end
 
   def fair
     lists = JSON.parse(params.as_json["parties"])
     is_fair = PokemonInfo.is_trade_fair? lists[0], lists[1]
-    data = {
+    render :json => {
       :error => nil,
       :result => is_fair,
     }
-    render :json => data
+  end
+
+  def save
+    render :json => {
+      :error  => Trade.save_lists(JSON.parse(params.as_json["parties"])),
+    }
   end
 end
