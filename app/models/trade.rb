75f5ce60dd486be9325b81b5ca98cbb6fd310d13
Cris Jr <crisjr@pm.me>
2021-04-27 11:22:45 -0300

    Update models and controllers to save and retrive trades

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,14 @@
 class Trade < ApplicationRecord
+  def to_view
+    return [
+      self.party1.split(","),
+      self.party2.split(","),
+    ]
+  end
+
+  def Trade.save_lists lists
+    party1 = lists[0].map{ |p| p.strip }.join(",")
+    party2 = lists[1].map{ |p| p.strip }.join(",")
+    Trade.create(:party1 => party1, :party2 => party2)
+  end
 end
